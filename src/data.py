5a96c6d11a48bdb3c74f7d27fad9e72027439cf8
w568w <1278297578@qq.com>
2023-05-26 20:01:55 +0800

    feat: impl transform filpping color jitting and tiling

--- src/data.py
+++ src/data.py
@@ -99,12 +99,84 @@ class ResizeImageWithGT(object):
         sample = {'image':resized_image,'boxes':boxes,'gt_density':resized_density}
         return sample
     
+class Filpping(object):
+    """
+    Flip the image horizontally with a probability of p
+    """
+    def __init__(self, p=0.5):
+        self.p = p
+
+    def __call__(self, sample):
+        image,lines_boxes,density = sample['image'], sample['lines_boxes'],sample['gt_density']
+        if random.random() < self.p:
+            image = transforms.functional.hflip(image)
+            density = torch.flip(density,[2])
+            boxes = list()
+            for box in lines_boxes:
+                y1, x1, y2, x2 = box[0], box[1], box[2], box[3]
+                boxes.append([y1,512-x2,y2,512-x1])
+            boxes = torch.Tensor(boxes)
+            sample = {'image':image,'boxes':boxes,'gt_density':density}
+        return sample
+
+class ColorJitting(object):
+    """
+    Randomly change the brightness, contrast and saturation of an image
+    """
+    def __init__(self, brightness=0.2, contrast=0.2, saturation=0.2):
+        self.brightness = brightness
+        self.contrast = contrast
+        self.saturation = saturation
+
+    def __call__(self, sample):
+        image,lines_boxes,density = sample['image'], sample['lines_boxes'],sample['gt_density']
+        brightness = random.uniform(1-self.brightness, 1+self.brightness)
+        contrast = random.uniform(1-self.contrast, 1+self.contrast)
+        saturation = random.uniform(1-self.saturation, 1+self.saturation)
+
+        image = transforms.functional.adjust_brightness(image, brightness)
+        image = transforms.functional.adjust_contrast(image, contrast)
+        image = transforms.functional.adjust_saturation(image, saturation)
+        sample = {'image':image,'boxes':lines_boxes,'gt_density':density}
+        return sample
+    
+class Tiling(object):
+    """
+    Randomly crop the image into [0.5-ratio, 0.5+ratio] of the original size
+    """
+    def __init__(self, p=0.5, ratio=0.1):
+        self.p = p
+        self.ratio = ratio
+
+    def __call__(self, sample):
+        image,lines_boxes,density = sample['image'], sample['lines_boxes'],sample['gt_density']
+        if random.random() < self.p:
+            W, H = image.size
+            w, h = int(W*self.ratio), int(H*self.ratio)
+            x1 = random.randint(0, W-w)
+            y1 = random.randint(0, H-h)
+            image = transforms.functional.crop(image, y1, x1, h, w)
+            density = density[:, y1:y1+h, x1:x1+w]
+            boxes = list()
+            for box in lines_boxes:
+                by1, bx1, by2, bx2 = box[0], box[1], box[2], box[3]
+                by1 = max(by1-y1, 0)
+                bx1 = max(bx1-x1, 0)
+                by2 = min(by2-y1, h)
+                bx2 = min(bx2-x1, w)
+                if by1 < by2 and bx1 < bx2:
+                    boxes.append([by1, bx1, by2, bx2])
+
+            sample = {'image':image,'boxes':lines_boxes,'gt_density':density}
+        return sample
+    
+
 normalize = transforms.Compose([transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
 def get_transform(split, wh=512):
     if split == 'train':
-        custom_transforms = transforms.Compose([ResizeImageWithGT(wh)])
+        custom_transforms = transforms.Compose([ResizeImageWithGT(wh), Filpping(), ColorJitting(), Tiling()])
     else:
         custom_transforms = transforms.Compose([ResizeImage(wh)])
     return custom_transforms
